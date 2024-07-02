64ac0afeb608d983a263f1e51b06d0ee70092ef9
박지현 <127105625+jihyun95732@users.noreply.github.com>
2024-07-02 14:59:30 +0900

    회원가입/로그인 페이지 기본구현

---
+++ lib/main/views/login.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:tech_lock/main/views/main_home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: const Color(0xFF000000),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: const Color(0xFF5DB075),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            primary: const Color(0xFFFFFFFF), // ElevatedButton 색상 설정
+          ),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black), // 비활성화 상태의 테두리 색상 설정
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Color(0xFF5DB075)), // 포커스 상태의 테두리 색상 설정
+          ),
+        ),
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          '로그인',
+          style: TextStyle(
+            fontSize: 30, // 원하는 글자 크기로 설정
+            fontWeight: FontWeight.w900, // 글씨 두껍게 설정
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Stack(
+        children: [
+          Container(
+            color: Colors.white, // 흰색 배경
+          ),
+          Positioned(
+            top: 30.0,
+            left: 20.0,
+            right: 20.0,
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: '이메일',
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                ),
+                const SizedBox(height: 20.0),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: '비밀번호',
+                  ),
+                  obscureText: true,
+                  keyboardType: TextInputType.visiblePassword,
+                  textInputAction: TextInputAction.done,
+                ),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const MyHomePage()),
+                    );
+                  },
+                  child: const Text('로그인'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
